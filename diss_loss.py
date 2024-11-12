e66c49adf672bf8e457b20bb2ef90728346b9af2
Sh1va5Chefs <ladyshiva0716@gmail.com>
2024-11-12 22:37:25 +0800

    Revert "fixed the diss_loss"
    This reverts commit 58b2d78168804ac0e04df2aaf1611b52bff4dcb8.

--- diss_loss.py
+++ diss_loss.py
@@ -1,20 +1,24 @@
 import torch 
 import torch.nn as nn
 import torch.nn.functional as F
+
 class DiceLoss(nn.Module):
-    def __init__(self, device):
-        super(DiceLoss, self).__init__()
-        self.smooth = 1e-5  
+    def __init__(self,device):
+        super(DiceLoss, self).__init__()  
+        self.smooth = 1e-5 #prevent 0 division
         self.device = device
         
-    def forward(self, predict, target):
+    def forward(self,predict,target):
         predict = predict.to(self.device)
         target = target.to(self.device)
 
-        intersection = torch.sum(predict * target, dim=(1, 2))  
-        union = torch.sum(predict, dim=(1, 2)) + torch.sum(target, dim=(1, 2))
+        target_onehot = torch.zeros(predict.size(), device=self.device)
+        target_onehot.scatter_(1, target.long(), 1) 
 
-        dice = (2 * intersection + self.smooth) / (union + self.smooth)
-        dice_loss = 1 - torch.mean(dice) 
+        intersection = torch.sum(predict*target_onehot,dim=2)
+        union = torch.sum(predict.pow(2),dim=2) +torch.sum(target_onehot,dim=2)
+
+        dice = (2*intersection+self.smooth) /(union+self.smooth)
+        dice_loss= 1-torch.mean(dice)
             
         return dice_loss
